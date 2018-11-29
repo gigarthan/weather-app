b74110dd42c9585c60f41d134e9d93c95074fe56
gigarthan <gigajpx@gmail.com>
2018-11-29 08:28:08 +0530

    Add basic models

---
+++ src/models/WeatherModel.ts
@@ -0,0 +1,9 @@
+export class WeatherModel {
+    maxTemp: Number;
+    minTemp: Number;
+    windSpeed: Number;
+    humidity: Number;
+    seaLevel: string;
+    weatherDesc: string;
+    windDegree: Number;
+}
