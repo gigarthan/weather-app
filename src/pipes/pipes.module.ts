07350ea36b6f5e351552ab1713741b09da40836d
gigarthan <gigajpx@gmail.com>
2018-11-29 08:27:02 +0530

    Add custom pipes module

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { DefaultPipe } from './default/default';
+@NgModule({
+	declarations: [DefaultPipe],
+	imports: [],
+	exports: [DefaultPipe]
+})
+export class PipesModule {}
