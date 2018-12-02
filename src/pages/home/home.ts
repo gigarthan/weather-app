170454a164c0f04fb747962d7f1f3280a0775571
gigarthan <gigajpx@gmail.com>
2018-12-02 10:33:21 +0530

    Add new changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,33 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
-import { WeatherServiceProvider } from '../../providers/weather-service/weather-service';
-import { Coordinate } from '../../models/CoordinatesModel';
-import { WeatherModel } from '../../models/WeatherModel';
+import { Component, OnInit } from "@angular/core";
+import { NavController, AlertController } from "ionic-angular";
+import { Geolocation } from "@ionic-native/geolocation";
+import { WeatherServiceProvider } from "../../providers/weather-service/weather-service";
+import { Coordinate } from "../../models/CoordinatesModel";
+import { WeatherModel } from "../../models/WeatherModel";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage implements OnInit {
-
   // Device geolocation
   coordinates: Coordinate;
+  displayCoordinates: Coordinate;
   country: string;
-  weatherData: WeatherModel
+  weatherData: WeatherModel;
+  hourlyForecast: Number;
 
   constructor(
-    public navCtrl: NavController, 
-    private location: Geolocation, 
-    private weather: WeatherServiceProvider ,
+    public navCtrl: NavController,
+    private location: Geolocation,
+    private weather: WeatherServiceProvider,
     private alertCtrl: AlertController
   ) {
     this.coordinates = new Coordinate();
+    this.displayCoordinates = new Coordinate();
     this.weatherData = new WeatherModel();
   }
 
   ngOnInit() {
-    // Load today's weather as default 
+    // Load today's weather as default
     // when the app starts
     this.loadWeather();
   }
@@ -36,31 +38,31 @@ export class HomePage implements OnInit {
    * Retrive Current Weather
    */
   loadWeather(): void {
+    this.getPosition().then(resp => {
+      this.coordinates.long = resp.long;
+      this.coordinates.lat = resp.lat;
 
-    this.getPosition()
-      .then(resp => {
-          this.coordinates.long = resp.long;
-          this.coordinates.lat  = resp.lat;
+      this.displayCoordinates.long = parseFloat(resp.long.toFixed(4));
+      this.displayCoordinates.lat = parseFloat(resp.lat.toFixed(4));
 
-          this.weather.getWeatherToday(this.coordinates)
-          .then( data => {
-              
-              this.country = data.country;
-              this.weatherData = data.weatherData;
-              
-          });
+      this.weather.getWeatherToday(this.coordinates).then(data => {
+        this.country = data.country;
+        this.weatherData = data.weatherData;
       });
-
+    });
   }
 
   loadWeatherTomorrow() {
-    this.getPosition()
-    .then(resp => {
-        this.coordinates.long = resp.long;
-        this.coordinates.lat  = resp.lat;
+    this.getPosition().then(resp => {
+      this.coordinates.long = resp.long;
+      this.coordinates.lat = resp.lat;
 
-        this.weather.getWeatherTomorrow(this.coordinates)
-        .then( weatherData => this.weatherData = weatherData );
+      this.displayCoordinates.long = parseFloat(resp.long.toFixed(4));
+      this.displayCoordinates.lat = parseFloat(resp.lat.toFixed(4));
+
+      this.weather
+        .getWeatherTomorrow(this.coordinates)
+        .then(weatherData => (this.weatherData = weatherData));
     });
   }
 
@@ -68,32 +70,60 @@ export class HomePage implements OnInit {
    * Get the device current position
    * and returns the longitude and latitude
    */
-  getPosition() : Promise<Coordinate> {
+  getPosition(): Promise<Coordinate> {
     return new Promise((resolve, reject) => {
-      this.location.getCurrentPosition()
+      this.location
+        .getCurrentPosition()
         .then(resp => {
-          
           let coordinates = new Coordinate();
           coordinates.long = resp.coords.longitude;
-          coordinates.lat  = resp.coords.latitude;
+          coordinates.lat = resp.coords.latitude;
 
           return resolve(coordinates);
         })
         .catch(err => {
-          console.log('ERROR', err);
-          
+          console.log("ERROR", err);
+
           const alert = this.alertCtrl.create({
-            title: 'Error!',
+            title: "Error!",
             subTitle: err.message,
-            buttons: ['OK']
+            buttons: ["OK"]
           });
 
           alert.present();
 
           return reject(err);
-
         });
-    })
+    });
   }
 
+  /**
+   * retrieve hourly forecast for the day
+   *
+   * 
+   */
+  hourlyRange() {
+    /**
+     * if the range is 0 we default back to
+     *  the current weather for the day
+     */
+    if (this.hourlyForecast === 0) {
+      this.loadWeather();
+    } else {
+      /** hourlyforacast is casted to a string and passed */
+      this.weather
+        .getHourlyForecast(this.coordinates, `${this.hourlyForecast}`)
+        .then(weatherData => (this.weatherData = weatherData));
+    }
+  }
+
+   debounce(fun, mil) {
+    var timer; 
+    return function(){
+        clearTimeout(timer); 
+        timer = setTimeout(function(){
+            fun(); 
+        }, mil); 
+    };
+}
 }
